f005f488eaf2bc648396d1926018660adf2b1eae
PetterKertpz <petterlopez103@gmail.com>
2025-09-13 02:05:59 -0500

    Iniciando integración de la estructura completa de la app a la db

--- app/src/main/java/com/example/freeplayerm/ui/features/biblioteca/components/ListaDeCanciones.kt
+++ app/src/main/java/com/example/freeplayerm/ui/features/biblioteca/components/ListaDeCanciones.kt
@@ -3,7 +3,6 @@ package com.example.freeplayerm.ui.features.biblioteca.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -11,19 +10,13 @@ import com.example.freeplayerm.ui.theme.AppColors
 
 @Composable
 fun ListaDeCanciones(modifier: Modifier = Modifier) {
-    Column (
+    Column(
         Modifier
             .fillMaxWidth()
-            .background(AppColors.GrisProfundo)
+            .background(AppColors.GrisProfundo),
+        content = TODO()
+    )
 
-
-
-    ) {
-        Text(
-            "LISTA",
-            color = AppColors.Negro,
-        )
-    }
 }
 
 @Composable
